d9ca12c3ffce44171ab57b023a17918e159ec958
johanREGIS <johanjregis@gmail.com>
2025-02-22 16:57:21 +0100

    Modifications du 22 février 2025 et Affichage des Users

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { User } from '../../models/user/user';
+import { UserService } from '../../services/user/user.service';
+import { NotificationService } from '../../services/notification/notification.service';
+import { NotificationType } from '../../enum/notification-type.enum';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -9,16 +15,63 @@ import { Router } from '@angular/router';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+  private titleSubject = new BehaviorSubject<string>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
+  private subscriptions:Subscription[] = [];
+  public users:User[] = [];
+  public refreshing:boolean = false;
+
 
   constructor(
     private authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private userService: UserService,
+    private notificationService: NotificationService
   ) {  }
+
+  ngOnInit(): void {
+    this.getUsers(true);
+  }
+
+  public changeTitle(title:string):void {
+    this.titleSubject.next(title);
+  }
 
   public logOut() {
     this.authenticationService.logout();
     this.router.navigateByUrl('login');
   }
   
+  public getUsers(showNotification:boolean):void {
+    this.refreshing = true;
+    this.subscriptions.push(
+      this.userService.getUsers().subscribe({
+        next:
+        (response:User[]) => {
+          this.userService.addUsersToLocalCache(response);
+          this.users = response;
+          this.refreshing = false;
+
+          if(showNotification) {
+            this.sendNotification(NotificationType.SUCCESS, `${response.length} user(s) loaded successfully`);
+          }
+        },
+        error:
+        (errorResponse:HttpErrorResponse) => {  // Error Response
+                  this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+                  this.refreshing = false;
+                }
+      })
+    );
+  }
+
+  sendNotification(notificationType: NotificationType, message: string):void {
+    if(message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, "AN ERROR OCCURED, PLEASE TRY AGAIN");
+    }
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../../settings/app.settings';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../../models/user/user';
+import { Observable } from 'rxjs';
+import { CustomHttpResponse } from '../../models/custom-http-response';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,22 @@ export class UserService {
   private host = AppSettings.APP_URL;
 
   constructor(private http:HttpClient) { }
+
+  public getUsers():Observable<User[]> {
+    return this.http.get<User[]>(`${this.host}api/auth/list`);
+  }
+
+  public addUser(formData:FormData):Observable<User> {
+    return this.http.post<User>(`${this.host}api/auth/add`, formData);
+  }
+
+  public updateUser(formData:FormData):Observable<User> {
+    return this.http.post<User>(`${this.host}api/auth/update`, formData);
+  }
+
+  public deleteUser(userId:number):Observable<CustomHttpResponse> {
+    return this.http.delete<CustomHttpResponse>(`${this.host}api/auth/delete/${userId}`);
+  }
 
   // Add user in cache
   public addUsersToLocalCache(users:User[]):void {
